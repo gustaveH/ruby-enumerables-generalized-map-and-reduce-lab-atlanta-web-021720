128fbc74d77ff48f28827e4db6c60c8146669505
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 03:04:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,13 +8,13 @@ def map (array)
   new 
 end 
 
-def reduce (array, sv = nil)
+def reduce(array, sv = nil)
   if sv 
     sum = sv 
     i=0 
   else 
     sum = array[0]
-    i=0 
+    i=1  
   end 
   while i < array.length 
     sum = yield(sum, array[i])
